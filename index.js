4853d264d82bee7322465a935b0a81c4f4b996f1
Tomáš Šíma <simatomas2000@gmail.com>
2022-11-13 22:37:59 +0100

    logika, nefunguje emoce a rating

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
 
 
 const db = new Dexie("Todo App");
-db.version(1).stores({ todos: "++id, todo, emoce, today, intenzita" });
+db.version(1).stores({ todos: "++id, today, time, task, with, emotions, rating, thoughts, body" });
 
 const form = document.querySelector("#new-task-form");
-const input = document.querySelector("#new-task-input");
-const emoce_i = document.querySelector("#emoce");
-const intenzita_i = document.querySelector("#intenzita");
+
+  const time = document.querySelector("#time");
+  const task = document.querySelector("#task");
+  const withh = document.querySelector("#with");
+  const emoce = document.querySelector("#emoce");
+  const rating = document.querySelector("#rating");
+  const thoughts = document.querySelector("#thoughts");
+  const bodyfeel = document.querySelector("#bodyfeel");
+
 const list_el = document.querySelector("#tasks");
+
+
 
 //add todo
 form.onsubmit = async (event) => {
   event.preventDefault();
-  const todo = input.value;
-  const emoce = emoce_i.value;
-  const intenzita = intenzita_i.value;
-  var today = new Date();
-  await db.todos.add({ todo,emoce,today,intenzita });
+  const timev = time.value;
+  const taskv = task.value;
+  const withhv = withh.value;
+  const emocev = emoce.value;
+  const ratingv = rating.value;
+  const thoughtsv = thoughts.value;
+  const bodyfeelv = bodyfeel.value;
+
+
+  
+  
+  var current = new Date();
+  const today = current.getDate().toString() + ":" + (current.getMonth()+1).toString()
+  await db.todos.add({today,  timev, taskv, withhv, emocev,ratingv,thoughtsv, bodyfeelv });
   await getTodos();
-  form.reset();
+  // form.reset();
 };
-document.getElementById("time").defaultValue = current.getHours();
+// document.getElementById("time").defaultValue = current.getHours();
 
 //display todo
+// Cítil jsem se ${todo.time} ${todo.task} ${todo.time.toLocaleDateString()} ${todo.today.toLocaleTimeString()}..
 const getTodos = async () => {
   const allTodos = await db.todos.reverse().toArray();
   list_el.innerHTML = allTodos
-    .map(
-      (todo) => `
+  .map(
+    (todo) => `
     
     <div class="task">
     <div class="content">
-    Cítil jsem se ${todo.intenzita} ${todo.emoce} ${todo.today.toLocaleDateString()} ${todo.today.toLocaleTimeString()}..
+    // Cítil jsem se ${todo.timev} ${todo.taskv} ${todo.withv}}..
+    // Cítil jsem se ${todo.emocev} ${todo.ratingv} ${todo.thoughtsv}}..
     </div>
     <div class="content2">
     ${todo.todo}
@@ -43,11 +62,11 @@ const getTodos = async () => {
     `
     )
     .join("");
-};
-window.onload = getTodos;
-
-//delete todo
-const deleteTodo = async ( id) => {
-  await db.todos.delete(id);
-  await getTodos();
-};
+  };
+  window.onload = getTodos;
+  
+  //delete todo
+  const deleteTodo = async ( id) => {
+    await db.todos.delete(id);
+    await getTodos();
+  };
